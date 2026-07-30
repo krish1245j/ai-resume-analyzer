0162bf797f56f19ded95b37ec82fef7c2dc41f9d
Krish Kumar <krish1245j@gmail.com>
2026-07-31 00:31:27 +0530

    Restructure project into frontend and backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-resume-analyzer-ha5v.onrender.com/",
+  baseURL: "https://ai-resume-analyzer-ha5v.onrender.com/api",
   withCredentials: true,
 });
 
